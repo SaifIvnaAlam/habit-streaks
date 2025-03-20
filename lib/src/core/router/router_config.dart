5b36e827e31aa840dfc3f7ec0d31f32e66005de7
saif ivna alam <alam.saifivna@gmail.com>
2025-03-20 10:28:03 +0600

    add: new route

--- lib/src/core/router/router_config.dart
+++ lib/src/core/router/router_config.dart
@@ -1,7 +1,9 @@
 import 'package:go_router/go_router.dart';
+import 'package:habitstreaks/src/features/calendar/presentation/calendar_page.dart';
 import 'package:habitstreaks/src/features/create_habit/presentation/create_habit_page.dart';
 import 'package:habitstreaks/src/features/home/presentation/home_page.dart';
 import 'package:habitstreaks/src/features/profile/presentation/profile_page.dart';
+import 'package:habitstreaks/src/features/todo/presentation/create_todo_page.dart';
 import 'package:habitstreaks/src/features/wrapper/presentation/wrapper_page.dart';
 
 final router = GoRouter(
@@ -26,6 +28,16 @@ final router = GoRouter(
       name: ProfilePage.route,
       path: '/${ProfilePage.route}',
       builder: (context, state) => ProfilePage(),
+    ),
+    GoRoute(
+      name: CreateTodoPage.route,
+      path: '/${CreateTodoPage.route}',
+      builder: (context, state) => CreateTodoPage(),
+    ),
+    GoRoute(
+      name: CalendarPage.route,
+      path: '/${CalendarPage.route}',
+      builder: (context, state) => CalendarPage(),
     ),
   ],
 );
