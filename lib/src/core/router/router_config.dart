268a759a312c9a795411861344385bb8d60b2747
saif ivna alam <alam.saifivna@gmail.com>
2025-03-20 09:45:27 +0600

    add: new pages

--- lib/src/core/router/router_config.dart
+++ lib/src/core/router/router_config.dart
@@ -1,6 +1,8 @@
 import 'package:go_router/go_router.dart';
 import 'package:habitstreaks/src/features/create_habit/presentation/create_habit_page.dart';
 import 'package:habitstreaks/src/features/home/presentation/home_page.dart';
+import 'package:habitstreaks/src/features/profile/presentation/profile_page.dart';
+import 'package:habitstreaks/src/features/wrapper/presentation/wrapper_page.dart';
 
 final router = GoRouter(
   initialLocation: '/',
@@ -8,12 +10,22 @@ final router = GoRouter(
     GoRoute(
       name: HomePage.route,
       path: '/',
-      builder: (context, state) => const HomePage(),
+      builder: (context, state) => HomePage(),
     ),
     GoRoute(
       name: CreateHabitPage.route,
       path: '/${CreateHabitPage.route}',
       builder: (context, state) => CreateHabitPage(),
+    ),
+    GoRoute(
+      name: WrapperPage.route,
+      path: '/${WrapperPage.route}',
+      builder: (context, state) => WrapperPage(),
+    ),
+    GoRoute(
+      name: ProfilePage.route,
+      path: '/${ProfilePage.route}',
+      builder: (context, state) => ProfilePage(),
     ),
   ],
 );
