db048aabcf8e3fa8e235cda79ff46de94c181542
saif ivna alam <alam.saifivna@gmail.com>
2025-03-20 09:48:21 +0600

    add: create todo page

---
+++ lib/src/features/todo/presentation/create_todo_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreateTodoPage extends StatelessWidget {
+  const CreateTodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("quick action: Create todo page"),
+      ),
+    );
+  }
+}
