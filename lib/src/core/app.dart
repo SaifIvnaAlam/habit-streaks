f78a84ea03865fbedb2c94cd40517a9c453eca7a
saif ivna alam <alam.saifivna@gmail.com>
2025-03-20 09:46:18 +0600

    update: Theme style

--- lib/src/core/app.dart
+++ lib/src/core/app.dart
@@ -3,6 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habitstreaks/src/core/router/router_config.dart';
 import 'package:habitstreaks/src/features/create_habit/application/cubit/create_habit_cubit.dart';
 import 'package:habitstreaks/src/features/home/application/cubit/load_habits_cubit.dart';
+import 'package:habitstreaks/src/shared/app_colors.dart';
+import 'package:habitstreaks/src/shared/app_text_style.dart';
+import 'package:habitstreaks/src/shared/custom_color_scheme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -21,6 +24,15 @@ class App extends StatelessWidget {
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
+          primaryColor: AppColors.primary, // Example background color
+          textTheme: TextTheme(
+            displayLarge: AppTextStyles.title,
+            displayMedium: AppTextStyles.subtitle,
+            bodyLarge: AppTextStyles.bodyText,
+            labelLarge: AppTextStyles.buttonText,
+            bodySmall: AppTextStyles.captionText,
+          ),
+          colorScheme: customColorScheme,
           useMaterial3: true,
         ),
         routerConfig: router,
